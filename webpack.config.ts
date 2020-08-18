e04f7d5a8f5ed4295f62b20dea9046392634ca2a
meixg <meixg@foxmail.com>
2020-08-18 13:44:52 +0800

    build: use webpack

---
+++ webpack.config.ts
@@ -0,0 +1,36 @@
+import * as webpack from 'webpack';
+import * as path from 'path';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+
+const config: webpack.Configuration = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-typescript']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+};
+
+export default config;
